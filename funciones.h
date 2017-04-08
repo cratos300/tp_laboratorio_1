31962756039312bd48fecde161ddb38ec1f7732d
cratos300 <hernan-vilar@hotmail.com>
2017-04-07 22:36:52 -0300

    Trabajo practico

---
+++ funciones.h
@@ -0,0 +1,38 @@
+int suma (int,int);
+/** \brief : Suma dos numeros
+ *
+ * \param : Digite un numero
+ * \param : Digite otro numero
+ * \return : Suma ambos numeros
+ *
+ */
+ int resta (int,int);
+/** \brief : Resta dos numeros
+ *
+ * \param : Digine un numero
+ * \param : Digite otro numero
+ * \return : Resta ambos numeros
+ *
+ */
+
+
+ int division (int,int);
+ /** \brief : divide dos numeros
+  *
+  * \param : Digite un numero
+  * \param : Digite otro numero
+  * \return : Divide ambos numeros
+  *
+  */
+int multiplicacion (int,int);
+ /** \brief : multiplica dos numeros
+ *
+ * \param : Digite un numero
+ * \param : Digite otro numero
+ * \return : Multiplica ambos numeros
+ *
+ */
+
+
+
+
